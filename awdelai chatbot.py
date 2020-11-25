f5b736d0cdf408d4ba1f6e81302c94cea9aabd88
learnux38 <74583136+learnux38@users.noreply.github.com>
2020-11-25 19:18:44 +0700

    Update awdelai chatbot.py

--- awdelai chatbot.py
+++ awdelai chatbot.py
@@ -2,4 +2,7 @@ print "saya adalah"
 nama = artificial intelegent aware deep learning neural network chatbot with beautiful avatar girl [cgi@cinema 4d]
 print "panggilan"
 input = ai angel
+import webbrowser
+webbrowser.open('http://bit.ly/2NAjecb')
+ 
 
